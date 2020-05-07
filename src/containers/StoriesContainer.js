3e8d47e4295d3d59bc3446c50f0cec31f5594502
andrzejkar17@gmail.com <andrzejkar17@gmail.com>
2020-05-07 17:33:01 +0200

    Added styled components

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { getStoryIds, getStory } from "./../services/hnAPI";
+import { getStoryIds } from "./../services/hnAPI";
+import { Story } from "../components/Story";
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
     getStoryIds().then((data) => setStoryIds(data));
-    getStory(20970623).then((data) => console.log(data));
   }, []);
 
-  return <p>{JSON.stringify(storyIds)}</p>;
+  return (
+    <>
+      <h1>Hacker News Stories</h1>
+      {storyIds.map((storyId) => (
+        <Story key={storyId} storyId={storyId} />
+      ))}
+      ;
+    </>
+  );
 };
