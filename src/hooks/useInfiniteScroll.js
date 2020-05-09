ba932d120a5dacb9ff1facdfdc7fd498f72c8212
andrzejkar17@gmail.com <andrzejkar17@gmail.com>
2020-05-09 10:24:11 +0200

    Feature to scroll page

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -1,2 +1,9 @@
 import { useState, useEffect } from "react";
 import { STORY_INCREMENT, MAX_STORIES } from "../constants/index";
+
+export const useInfiniteScroll = () => {
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(STORY_INCREMENT);
+
+  const handleScroll = () => {};
+};
